13d5eb7f36e1d161f6d5a1c8b604cdbccb73c782
bishaorl <bishamoten2024@outlook.com>
2025-12-11 13:03:19 -0400

    Home y login totalmente responsive y funcionando

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { login, reset } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 import { toast } from "react-toastify";
 import "../styles/login.css";
 
@@ -10,7 +10,7 @@ const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { user, error, success, message } = useSelector((state) => state.auth);
+  // const { user, error, success, message } = useSelector((state) => state.auth);
 
   const openLoginModal = () => {
     setShowLoginModal(true);
@@ -44,7 +44,7 @@ const Home = () => {
       dispatch(login(userData)).then((result) => {
         if (result.meta.requestStatus === 'fulfilled') {
           // Login exitoso
-          toast.success('Inicio de Sesión Exitoso');
+          // toast.success('Inicio de Sesión Exitoso');
           closeLoginModal();
           navigate("/dashboard");
         } else {
@@ -88,9 +88,9 @@ const Home = () => {
             Iniciar Sesión
           </button>
 
-          <div className="login-modal-footer">
+          {/* <div className="login-modal-footer">
             <a href="/">Regresar a Página de Inicio</a>
-          </div>
+          </div> */}
         </form>
       </div>
     );
@@ -136,7 +136,7 @@ const Home = () => {
             
             <div className="hero-description">
               <p>
-                - Interfaz fácil de administrar.
+                - Interfaz Intuitiva y fácil de administrar.
                 <br />- Capacidad para rastrear y categorizar productos fácilmente. 
                 <br />- Creando pedidos y facturación pedidos creados.
                 <br />- Posibilidad de editar y eliminar productos y pedidos según
@@ -152,7 +152,7 @@ const Home = () => {
           
           <div className="hero-image">
             <img 
-              src={require("../images/pos-bg.png")} 
+              src={require("../images/Bm9X.gif")} 
               alt="Sistema punto de ventas" 
               className="background-img"
             />
